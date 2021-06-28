b51b1c2ed49704f6304715a36ff3f8a0dff396b6
Stephen Arancio <sjarancio@gmail.com>
2021-06-28 18:15:51 -0400

    commit tree dd780b67444b7dd9979dfec2863b2220ec3089a3

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // Write your code here
+
+// const multiply = function(num1, num2){
+//     num1 * num2
+// }
+
+// multiply(31,2);
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+
+let num3 = 31
+let num4 = 9
+
+let mod = (num3 % num4)
+
+
+
+const max = Math.max(1, 2, 3, 4, 5, 15, 20);
